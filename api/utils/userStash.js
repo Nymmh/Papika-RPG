ce16e6e1f279d17180d868f6f962fd1372fe3389
Nymh <stickman7415web@gmail.com>
2021-04-08 12:19:58 -0600

    added take items from house

--- api/utils/userStash.js
+++ api/utils/userStash.js
@@ -42,6 +42,42 @@ function userStash(auth,discordId,item,amount,reason){
                     }
                 });
             });
+        }else if(reason == 'take'){
+            UserInventories.findOne({discordId:discordId},(err,res)=>{
+                let usedspace = (res.usedSpace+amount),
+                    groceries = (res.groceries+amount),
+                    fastfood = (res.fastfood+amount);
+                UserHouseInventory.findOne({discordId:discordId},(err,res)=>{
+                    let houseUsedSpace = (res.usedSpace-amount),
+                        housegroceries = 0,
+                        housefastfood = 0;
+                    if(res.groceries)housegroceries = res.groceries;
+                    if(res.fastfood)housefastfood = res.fastfood;
+                    housegroceries = (housegroceries-amount);
+                    housefastfood = (housegroceries-amount);
+                    UserInventories.findOneAndUpdate({discordId:discordId},{usedSpace:usedspace},{new:true},(err,data)=>{
+                        if(err)console.log(err);else console.log(data)
+                    });
+                    UserHouseInventory.findOneAndUpdate({discordId:discordId},{usedSpace:houseUsedSpace},{new:true},(err,data)=>{
+                        if(err)console.log(err);else console.log(data)
+                    });
+                    if(item === "Groceries"){
+                        UserInventories.findOneAndUpdate({discordId:discordId},{groceries:groceries},{new:true},(err,data)=>{
+                            if(err)console.log(err);else console.log(data)
+                        });
+                        UserHouseInventory.findOneAndUpdate({discordId:discordId},{groceries:housegroceries},{new:true},(err,data)=>{
+                            if(err)console.log(err);else console.log(data)
+                        });
+                    }else if(item === "Fast_Food"){
+                        UserInventories.findOneAndUpdate({discordId:discordId},{fastfood:fastfood},{new:true},(err,data)=>{
+                            if(err)console.log(err);else console.log(data)
+                        });
+                        UserHouseInventory.findOneAndUpdate({discordId:discordId},{fastfood:housefastfood},{new:true},(err,data)=>{
+                            if(err)console.log(err);else console.log(data)
+                        });
+                    }
+                });
+            });
         }
     }
 }
---
+++ bot/commands/all/take.js
@@ -0,0 +1,96 @@
+const handleError = require('../../util/utils.js').handleError,
+      axios = require('axios');
+      config = require('../../json/config.json'),
+      utils = require('../../util/utils');
+module.exports = {
+    desc: "Take an item from your house",
+    aliases: ['tk'],
+    usage: "[item] <amount>",
+    cooldown: 5,
+    guildOnly: true,
+    channel: "house",
+    task(Ai, msg, suffix){
+        if(!suffix) return 'wrong usage';
+        let options = suffix.split(" ");
+        if(options.length>=0){
+            let option = options[0];
+            if(options.length == 3)option = options[0].concat(' '+options[1]);
+            let amount = 1;
+            if(options[2])amount = Number(options[2]);
+            else if(options[1])amount = Number(options[1]);
+            if(amount == 0)return Ai.createMessage(msg.channel.id,`<@${msg.author.id}>, You can not take 0 of an item.`).catch(err => {handleError(Ai, __filename, msg.channel, err)});
+            if(amount % 1 != 0)return Ai.createMessage(msg.channel.id,`<@${msg.author.id}>, You can only take whole numbers of items.`).catch(err => {handleError(Ai, __filename, msg.channel, err)});
+            let optionFix = option.replace(/(_)/g,' ').toLowerCase().replace(/(^\w{1})|(\s+\w{1})/g,lt=>lt.toUpperCase()).replace(/( )/g,'_');
+            axios({
+                url:config.APIurl,
+                method:'post',
+                data:{
+                    query:`
+                    query($name:String){
+                        items(name:$name){
+                            price
+                        }
+                      }
+                    `,
+                    variables:{
+                        name:optionFix
+                    },
+                    headers:{
+                        'Content-Type':'application/json'
+                    },
+                }
+            }).then(result=>{
+                if(!result.data.data.items[0])return Ai.createMessage(msg.channel.id,`<@${msg.author.id}>,`+" I could not find an item called ``"+optionFix+"``.").catch(err => {handleError(Ai, __filename, msg.channel, err)});
+                axios({
+                    url:config.APIurl,
+                    method:'post',
+                    data:{
+                        query:`
+                        query($discordID:String){
+                            users(discordId:$discordID){
+                                inventory{
+                                    usedSpace
+                                    maxSpace
+                                }
+                            }
+                          }
+                        `,
+                        variables:{
+                            discordID:msg.author.id
+                        },
+                        headers:{
+                            'Content-Type':'application/json'
+                        },
+                    }
+                }).then(result=>{
+                    if((result.data.data.users[0].inventory[0].usedSpace+amount)>result.data.data.users[0].inventory[0].maxSpace)return Ai.createMessage(msg.channel.id,`<@${msg.author.id}>, you dont have enough inventory space.`).catch(err => {handleError(Ai, __filename, msg.channel, err)});
+                    axios({
+                        url:config.APIurl,
+                        method:'post',
+                        data:{
+                            query:`
+                            mutation($auth:String,$discordId:String,$item:String,$amount:Int,$reason:String){
+                                userStoreItem(auth:$auth,discordId:$discordId,item:$item,amount:$amount,reason:$reason){
+                                  discordId
+                                }
+                              }
+                            `,
+                            variables:{
+                                auth:config.APIAuth,
+                                discordId:msg.author.id,
+                                item:optionFix,
+                                amount:amount,
+                                reason:"take"
+                            },
+                            headers:{
+                                'Content-Type':'application/json'
+                            },
+                        }
+                    }).then(()=>{
+                        return Ai.createMessage(msg.channel.id,`<@${msg.author.id}>, you took ${amount} ${optionFix} from your house.`).catch(err => {handleError(Ai, __filename, msg.channel, err)});
+                    })
+                })
+            })
+        }
+    }
+}
